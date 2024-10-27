e43410f8ae49cf7a86aaf0b45e08a58f6d198c8f
Noel De Martin <noel@noeldemartin.com>
2024-10-27 11:25:42 +0700

    Configure PWA

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,6 +6,7 @@ import iconsResolver from 'unplugin-icons/resolver';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const basePath = isProduction ? '/vivant/' : undefined;
@@ -24,6 +25,35 @@ export default defineConfig({
         icons({
             iconCustomizer(_, __, props) {
                 props['aria-hidden'] = 'true';
+            },
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Vivant Playground',
+                short_name: 'Vivant Playground',
+                description: 'Make your applications feel alive',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'web-app-manifest-192x192',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'web-app-manifest-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            workbox: {
+                additionalManifestEntries: [
+                    '/apple-touch-icon.png',
+                    '/favicon-48x48.png',
+                    '/favicon.ico',
+                    '/favicon.svg',
+                ],
             },
         }),
     ],
